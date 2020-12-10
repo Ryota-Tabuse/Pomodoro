b9b678d4ea233d19e7ee9cb652872d321a111f36
RyotaTabuse <40317216+Ryota-Tabuse@users.noreply.github.com>
2020-12-11 00:56:47 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const ONE_MSEC = 1000;
 
 const countDownTimer = document.getElementById('timer');
 //初期表示
-Notification.requestPermission();
 countDownTimer.innerHTML = `${TIME_MIN.toString().padStart(2,'0')}：${TIME_SEC.toString().padStart(2,'0')}`;
 
 // 1000ミリ秒ごとに、実行する。
@@ -33,10 +32,6 @@ const CountDown = () => {
     if(TIME_MIN === 0 && TIME_SEC === 0 ) {
         // タイマー解除
         stopTimer();
-        Notification
-        .requestPermission()
-        .then(() => {
-            const notification = new Notification("Hello, world!");
-        });
+        Push.create('更新情報');
     }
 }
